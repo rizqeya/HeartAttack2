0c4b90cedbca707ce9bdfcad816a175f4fad3297
Rizqeya Irfan <macbook@Irfans-Laptop.local>
2022-07-14 19:13:01 +0700

    Pushing all files to git

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import prediction, recommendation # import your app modules here
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Prediction", prediction.app)
+app.add_app("Recommendation", recommendation.app)
+
+# The main app
+app.run()
